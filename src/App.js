1b50e5b5a6f0d9a927ad52af3012cd97eef834b2
Javier Gomez <javier.gov@outlook.es>
2024-11-22 23:18:43 -0600

    Estado (hooks) pt2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h1>El tado en React - Hook State</h1>
+        <h1>Estado en React - Hook State</h1>
         <PrimerEstado />
       </header>
     </div>
--- src/Components/PrimerEstado.js
+++ src/Components/PrimerEstado.js
@@ -3,11 +3,16 @@ import React from 'react'
 export const PrimerEstado = () => {
 
     let nombre = "Xavier Gómez"
+    
+    const cambiarNombre = () => {
+      nombre = "Juan Fernandez"
+    }
 
   return (
     <div>
         <h3>Componente : Primer estado</h3>
         <b> {nombre} </b>
+        <button onClick={cambiarNombre}>Cambiar</button>
     </div>
   )
 }
