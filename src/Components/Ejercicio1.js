a6711114532d8b3e9dd86b4bcc481e467d309ce3
Javier Gomez <javier.gov@outlook.es>
2024-12-07 15:37:42 -0600

    botones funcionales

--- src/Components/Ejercicio1.js
+++ src/Components/Ejercicio1.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export const Ejercicio1 = ({year}) => {
+
+  const [yearNow, setYearNow] = useState(year)
+
+  const Siguiente = e => {
+    let operacion = yearNow + 1
+    setYearNow(operacion)
+  }
+
+  const Anterior = e => {
+    let operacion = yearNow - 1
+    setYearNow(operacion)
+  }
+
   return (
     <div>
         <h2 className=''>Ejercicio con eventos y estados</h2>
 
         <div className='label bg-slate-700 mt-5 rounded-lg py-3'>
-          <b>{year}</b>
+          <b>{yearNow}</b>
         </div>
 
         <div className="button__container flex gap-9 items-center justify-center mt-4">
-          <button className='button bg-cyan-400 py-1 px-3 rounded-md'>Siguiente</button>
-          <button className='button bg-cyan-400 py-1 px-3 rounded-md'>Anterior</button>
+
+        <button className='button bg-cyan-400 py-1 px-3 rounded-md' onClick={Anterior}>Anterior</button>
+
+          <button className='button bg-cyan-400 py-1 px-3 rounded-md' onClick={Siguiente}>Siguiente</button>
+         
         </div>
     </div>
   )
+ 
+
 }
+
+
