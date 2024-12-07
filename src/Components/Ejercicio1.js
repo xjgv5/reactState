facccfa98f7215996b2ee43bf09478842826dc97
Javier Gomez <javier.gov@outlook.es>
2024-12-06 22:33:30 -0600

    Botones

--- src/Components/Ejercicio1.js
+++ src/Components/Ejercicio1.js
@@ -6,9 +6,14 @@ export const Ejercicio1 = ({year}) => {
     <div>
         <h2 className=''>Ejercicio con eventos y estados</h2>
 
-        <p className='label bg-slate-700 mt-5 rounded-lg py-3'>
+        <div className='label bg-slate-700 mt-5 rounded-lg py-3'>
           <b>{year}</b>
-        </p>
+        </div>
+
+        <div className="button__container flex gap-9 items-center justify-center mt-4">
+          <button className='button bg-cyan-400 py-1 px-3 rounded-md'>Siguiente</button>
+          <button className='button bg-cyan-400 py-1 px-3 rounded-md'>Anterior</button>
+        </div>
     </div>
   )
 }
